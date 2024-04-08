5419dbd93a25d98a00e030c565d180ce33994743
whiteand <andrewbeletskiy@gmail.com>
2024-04-08 15:52:39 +0300

    fix: added forwarding of result error

--- src/encode.ts
+++ src/encode.ts
@@ -1,11 +1,17 @@
+import { ErrResult, Result } from "resultra";
 import { Uint8ArrayWriter } from "./defaults/Uint8ArrayWriter";
 import { Encoder } from "./Encoder";
 import { IEncoder } from "./IEncoder";
 
-export function encode(cb: (encoder: IEncoder) => void): Uint8Array {
+export function encode(
+  cb: (encoder: IEncoder) => void | Result<any>
+): Uint8Array {
   const buffer = new Uint8ArrayWriter(new Uint8Array(1024), { growable: true });
   const encoder = new Encoder(buffer);
-  cb(encoder);
+  let res = cb(encoder);
+  if (res && "ok" in res && !res.ok() && res.error) {
+    throw res.error;
+  }
   const bytes = buffer.intoUint8Array();
   return bytes;
 }
