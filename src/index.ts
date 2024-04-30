50779ec139059d5a44c66f48f9a1029399ae89ba
whiteand <andrewbeletskiy@gmail.com>
2024-04-30 09:45:46 +0300

    feat: added array and bytes to lib exports

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,11 @@ export { Encoder } from "./Encoder";
 export { CborType } from "./base";
 export { u8, u16, u32 } from "./default/smallInts";
 export { u64, u128 } from "./default/bigInts";
+export { bytes } from "./default/bytes";
 export { flatMap } from "./operators/flatMap";
 export { map } from "./operators/map";
 export { cast } from "./operators/cast";
+export { array } from "./operators/array";
 export {
   IDecoder,
   IEncoder,
