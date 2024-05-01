782d4898ef75feb1f086c95e3663d9d84659beab
whiteand <andrewbeletskiy@gmail.com>
2024-05-01 13:19:16 +0300

    test: added tests for null and undefined

--- src/default/unit.ts
+++
@@ -1,9 +0,0 @@
-import { CborType } from "../base";
-import { okNull } from "../okNull";
-
-const doNothingResult = () => okNull;
-
-export const unit = new CborType<null, unknown, never, unknown, never>(
-  doNothingResult,
-  doNothingResult,
-);
