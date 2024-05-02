0397e6d9ddd8a9060a64a3857a81974b58796c2d
whiteand <andrewbeletskiy@gmail.com>
2024-05-02 15:06:45 +0300

    feat: fixed cast

--- src/operators/cast.ts
+++ src/operators/cast.ts
@@ -1,13 +1,13 @@
 import { CborType } from "../base";
 import { ICborType } from "../types";
 
-export function cast<To, From extends To = To>(): <EE, DE>(
+export function cast<From, To extends From>(): <EE, DE>(
   ty: ICborType<From, void, EE, void, DE>,
 ) => CborType<To, void, EE, void, DE>;
-export function cast<To, From extends To = To>(): <EC, EE, DC, DE>(
+export function cast<From, To extends From>(): <EC, EE, DC, DE>(
   ty: ICborType<From, EC, EE, DC, DE>,
 ) => CborType<To, EC, EE, DC, DE>;
-export function cast<To, From extends To = To>(): <EE, DE>(
+export function cast<From, To extends From>(): <EE, DE>(
   ty: ICborType<From, any, EE, any, DE>,
 ) => CborType<To, any, EE, any, DE> {
   return <EE, DE>(ty: ICborType<From, any, EE, any, DE>) =>
