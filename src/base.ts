d195150b0194f33eef0cf452dbeae30048941b65
whiteand <andrewbeletskiy@gmail.com>
2024-05-02 16:32:46 +0300

    feat: improved from function of the CborType, to be able to infer contextless types

--- src/base.ts
+++ src/base.ts
@@ -18,6 +18,7 @@ import {
   TEncodeFunction,
 } from "./types";
 import { Pipeable } from "./pipe";
+import { u8 } from "./default/smallInts";
 
 export class CborType<T, EC, EE, DC, DE>
   extends Pipeable
@@ -47,15 +48,35 @@ export class CborType<T, EC, EE, DC, DE>
     return this[encodeSymbol](value, e, ctx);
   }
 
+  static from<T, EE, DE>(
+    enc: (value: T, encoder: IEncoder) => Result<void, EE>,
+    dec: (d: IDecoder) => Result<T, DE>,
+  ): CborType<T, void, EE, void, DE>;
+  static from<T, EE, DC, DE>(
+    enc: (value: T, encoder: IEncoder) => Result<void, EE>,
+    dec: TDecodeFunction<T, DC, DE>,
+  ): CborType<T, void, EE, DC, DE>;
+  static from<T, EC, EE, DE>(
+    enc: TEncodeFunction<T, EC, EE>,
+    dec: (d: IDecoder) => Result<T, DE>,
+  ): CborType<T, EC, EE, void, DE>;
   static from<T, EC, EE, DC, DE>(
-    t: ICborType<T, EC, EE, DC, DE>,
-  ): CborType<T, EC, EE, DC, DE> {
-    if (t instanceof CborType) {
-      return t;
+    enc: TEncodeFunction<T, EC, EE>,
+    dec: TDecodeFunction<T, DC, DE>,
+  ): CborType<T, EC, EE, DC, DE>;
+  static from<T, EC, EE, DC, DE>(
+    ty: ICborType<T, EC, EE, DC, DE>,
+  ): CborType<T, EC, EE, DC, DE>;
+  static from(encOrTy: any, dec?: any): any {
+    if (encOrTy instanceof CborType) {
+      return encOrTy;
+    }
+    if (typeof encOrTy === "function" && typeof dec === "function") {
+      return new CborType(encOrTy, dec);
     }
     return new CborType(
-      (v, e, c) => t[encodeSymbol](v, e, c),
-      (d, c) => t[decodeSymbol](d, c),
+      (v, e, c) => encOrTy[encodeSymbol](v, e, c),
+      (d, c) => encOrTy[decodeSymbol](d, c),
     );
   }
 }
