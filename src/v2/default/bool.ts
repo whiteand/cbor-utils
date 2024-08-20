ea4e5781c6041b366d514bb65ec70c4917ed9f0f
whiteand <andrewbeletskiy@gmail.com>
2024-08-20 10:55:24 +0300

    feat: added bool

---
+++ src/v2/default/bool.ts
@@ -0,0 +1,48 @@
+import { ok } from "resultra";
+import { getEoiResult, EndOfInputError } from "../EndOfInputError";
+import { TypeMismatchError } from "../TypeMismatchError";
+import { SPECIAL_TYPE_MASK } from "../constants";
+import { getTypeString } from "../getTypeString";
+import { CborType } from "../base";
+import { done } from "../utils/done";
+import { getVoidOk } from "../getVoidOk";
+import { getInfo } from "../marker";
+
+/**
+ * A CBOR type that encodes booleans
+ */
+export const bool: CborType<
+  boolean,
+  boolean,
+  TypeMismatchError,
+  EndOfInputError | TypeMismatchError,
+  unknown,
+  unknown
+> = CborType.builder()
+  .encode((v: boolean, e) => {
+    if (typeof v !== "boolean") {
+      return new TypeMismatchError("boolean", String(v)).err();
+    }
+
+    e.write(SPECIAL_TYPE_MASK | (v ? 21 : 20));
+    return getVoidOk();
+  })
+  .decode((d) => {
+    if (done(d)) {
+      return getEoiResult();
+    }
+    const m = d.buf[d.ptr++];
+    switch (getInfo(m)) {
+      case 20:
+        return ok(false);
+      case 21:
+        return ok(true);
+      default:
+        d.ptr--;
+        return new TypeMismatchError(
+          "boolean",
+          getTypeString(d.buf[d.ptr])
+        ).err();
+    }
+  })
+  .build();
