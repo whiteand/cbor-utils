72edab88ccf871f65433554acb807e2ba663ff9e
whiteand <andrewbeletskiy@gmail.com>
2024-08-21 09:08:14 +0300

    feat: rewrote constant operator with prototype inheritance

---
+++ src/v2/operators/constant.d.ts
@@ -0,0 +1,30 @@
+import { CborType } from "../base";
+import { ICborTypeCodec } from "../types";
+import { UnexpectedValueError } from "../UnexpectedValueError";
+
+/**
+ *
+ * Example:
+ *
+ * ```ts
+ * // two is a CBOR type that encodes and decodes nubmer 2.
+ * const two = u8.pipe(constant(2))
+ * ```
+ *
+ * @param expectedValue Value of the source type
+ * @param isEqual Function tahat compares the expected value with the actual one
+ * @returns a operator that transforms a type into a constant value type
+ */
+declare function constant<In, const V extends In>(
+  expectedValue: V,
+  isEqual?: (exp: NoInfer<V>, b: NoInfer<In>) => boolean
+): <EE extends Error, DE extends Error, EC, DC>(
+  ty: ICborTypeCodec<In, In, EE, DE, EC, DC>
+) => CborType<
+  V,
+  V,
+  EE | UnexpectedValueError<In, V>,
+  DE | UnexpectedValueError<In, V>,
+  EC,
+  DC
+>;
---
+++ src/v2/operators/constant.js
@@ -0,0 +1,57 @@
+import { ok } from "resultra";
+import { CborType } from "../base";
+import { UnexpectedValueError } from "../UnexpectedValueError";
+
+/**
+ *
+ * Example:
+ *
+ * ```ts
+ * // two is a CBOR type that encodes and decodes nubmer 2.
+ * const two = u8.pipe(constant(2))
+ * ```
+ *
+ * @param expectedValue Value of the source type
+ * @param isEqual Function tahat compares the expected value with the actual one
+ * @returns a operator that transforms a type into a constant value type
+ */
+export function constant(
+  expectedValue, isEqual = Object.is
+) {
+  return (ty) => {
+    const proto = CborType.builder()
+      .encode(
+        function encode(value, e, ctx) {
+          const { expectedValue } = this
+          if (!this.isEqual(value, expectedValue)) {
+            return new UnexpectedValueError(expectedValue, value).err();
+          }
+          return ty.encode(value, e, ctx);
+        }
+      )
+      .decode(function decode(d, ctx) {
+
+        const v = ty.decode(d, ctx);
+        if (!v.ok()) {
+          return v;
+        }
+        const { expectedValue } = this
+        if (!this.isEqual(expectedValue, v.value)) {
+          return new UnexpectedValueError(expectedValue, v.value).err();
+        }
+        return ok(v.value);
+      })
+      .build()
+
+    const constantType = {
+      expectedValue,
+      isEqual,
+    }
+
+    Reflect.setPrototypeOf(constantType, proto)
+
+    return constantType
+
+  }
+
+}
