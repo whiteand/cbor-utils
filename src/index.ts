7caec00d59e02c6cbc7d27e87380179c914498c8
whiteand <andrewbeletskiy@gmail.com>
2025-03-13 15:58:37 +0200

    feat: added import type

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,22 @@ export type { Result } from 'resultra'
 export { CborType } from "./base";
 export { decode, tryDecode } from "./decode";
 export { Decoder } from "./Decoder";
-export { DecodingError } from "./DecodingError";
+export type { DecodingError } from "./DecodingError";
 export { any } from "./default/any";
 export { arrayLen } from "./default/arrayLen";
 export { u128, u64 } from "./default/bigInts";
 export { bignum } from "./default/bignum";
 export { bool } from "./default/bool";
 export { bytes } from "./default/bytes";
-export { DataItem } from "./default/DataItem";
+export type { DataItem } from "./default/DataItem";
 export { f16 } from "./default/f16";
 export { f32 } from "./default/f32";
 export { f64 } from "./default/f64";
 export { map } from "./default/map";
 export { mapLen } from "./default/mapLen";
 export { nullType } from "./default/null";
-export { InferDecodedSeqType, InferEncodedSeqType, seq } from "./default/seq";
+export type { InferDecodedSeqType, InferEncodedSeqType } from "./default/seq";
+export { seq } from "./default/seq";
 export { u16, u32, u8 } from "./default/smallInts";
 export { str } from "./default/str";
 export { tuple } from "./default/tuple";
@@ -38,7 +39,7 @@ export { tagged } from "./operators/tagged";
 export { OverflowError } from "./OverflowError";
 export { skip } from "./skip";
 export { TypeMismatchError } from "./TypeMismatchError";
-export {
+export type {
   AnyDecodableType,
   AnyEncodableType,
   DecodeContext,
